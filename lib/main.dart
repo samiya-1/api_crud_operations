88d0369ea9d3ddb945ddd7bef4a4aa614b642e73
sameea <opsameea@gmail.com>
2024-06-05 09:49:22 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:api_crud_operations/controller/homescreencontroller.dart';
+import 'package:api_crud_operations/view/HomeScreen/homescreen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => HomeScreenController(),
+        )
+      ],
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
